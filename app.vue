9abd919aed84c8e37cbfd77ab4a6992095c64912
Gabriel Rosa <gabriel@rosa.dev.br>
2023-12-26 19:39:38 -0300

    🚀 Finalized

--- app.vue
+++ app.vue
@@ -3,6 +3,52 @@ const { y } = useWindowScroll()
 const { width } = useWindowSize()
 
 const menuOpen = ref(false)
+const yScrolled = ref(false)
+
+const navigation = ref([
+	{ title: 'Início' },
+	{ title: 'Sobre' },
+	{ title: 'Orçamento' },
+	{ title: 'Contato' },
+	{ title: 'Whatsapp', url: 'https://api.whatsapp.com/send?phone=5521983833874&text=Ol%C3%A1%20Hamilton,%20gostaria%20de%20contratar%20um%20servi%C3%A7o!' },
+	{ title: 'Facebook', url: 'https://www.facebook.com/Hamiltonmm' },
+])
+
+const current = ref('Início')
+
+const inicio = ref<HTMLElement | null>()
+const sobre = ref<HTMLElement | null>()
+const orcamento = ref<HTMLElement | null>()
+const contato = ref<HTMLElement | null>()
+
+function scrollTo(to: string) {
+	if (to === 'Início' && inicio.value)
+		window.scrollTo({ top: inicio.value.offsetTop, behavior: 'smooth' })
+	if (to === 'Sobre' && sobre.value)
+		window.scrollTo({ top: sobre.value.offsetTop, behavior: 'smooth' })
+	if (to === 'Orçamento' && orcamento.value)
+		window.scrollTo({ top: orcamento.value.offsetTop - 150, behavior: 'smooth' })
+	if (to === 'Contato' && contato.value)
+		window.scrollTo({ top: contato.value.offsetTop, behavior: 'smooth' })
+	menuOpen.value = false
+}
+
+function validateY(val: number) {
+	if (val > 50)
+		return yScrolled.value = true
+	yScrolled.value = false
+}
+
+function getActive(val: number) {
+	if (inicio.value && val >= inicio.value.offsetTop)
+		current.value = 'Início'
+	if (sobre.value && val >= sobre.value.offsetTop)
+		current.value = 'Sobre'
+	if (orcamento.value && val >= orcamento.value.offsetTop - 150)
+		current.value = 'Orçamento'
+	if (contato.value && val >= contato.value.offsetTop)
+		current.value = 'Contato'
+}
 
 watch(menuOpen, (nv) => {
 	if (nv)
@@ -17,21 +63,19 @@ watch(width, (nv) => {
 		document.body.style.overflow = 'hidden'
 })
 
-const navigation = ref([
-	{ title: 'Início' },
-	{ title: 'Sobre' },
-	{ title: 'Serviços' },
-	{ title: 'Fotos' },
-	{ title: 'Contato' },
-	{ title: 'Whatsapp', url: 'https://api.whatsapp.com/send?phone=5521983833874&text=Ol%C3%A1%20Hamilton,%20gostaria%20de%20contratar%20um%20servi%C3%A7o!' },
-	{ title: 'Facebook', url: 'https://www.facebook.com/Hamiltonmm' },
-])
+watch(y, (nv) => {
+	validateY(nv)
+	getActive(nv)
+})
 
-const current = ref('Início')
+onMounted(() => {
+	validateY(y.value)
+	getActive(y.value)
+})
 </script>
 
 <template>
-	<header class="h-[90px] transition-all duration-500 z-50 bg-transparent fixed top-0 right-0 left-0 flex items-center" :class="{ '!bg-[#343b40e6] !h-[70px]': y > 50 }">
+	<header class="h-[90px] transition-all duration-500 z-50 bg-transparent fixed top-0 right-0 left-0 flex items-center" :class="{ '!bg-[#343b40e6] !h-[70px]': yScrolled }">
 		<div class="container flex items-center justify-between mx-auto">
 			<NuxtImg src="/images/logo.png" alt="Hamilton Marcenaria Moderna" width="92" height="68" class="cursor-pointer" />
 			<nav :class="{ 'fixed overflow-hidden top-0 bottom-0 right-0 left-0 bg-[#4d4d4de6] transition-all duration-300 z-50': menuOpen && width < 1024 }">
@@ -40,7 +84,7 @@ const current = ref('Início')
 						<NuxtLink v-if="nav.url" :to="nav.url" external target="_blank" class="cursor-pointer flex items-center text-[14px] py-1 transition-all duration-300 relative uppercase before:content-[''] before:absolute before:w-full before:h-[2px] before:-bottom-[6px] before:left-0 before:bg-[#fc0324] before:invisible hover:before:visible before:scale-x-0 hover:before:scale-x-100 before:transition-all before:duration-300 text-white" :class="{ '!text-[#666666] !text-[15px] !px-[20px] !py-[10px] before:!bg-white hover:!text-[#fc0324]': menuOpen && width < 1024 }" @click="menuOpen = false">
 							{{ nav.title }}
 						</NuxtLink>
-						<p v-else class="cursor-pointer flex items-center text-[14px] py-1 transition-all duration-300 relative uppercase before:content-[''] before:absolute before:w-full before:h-[2px] before:-bottom-[6px] before:left-0 before:bg-[#fc0324] before:invisible hover:before:visible before:scale-x-0 hover:before:scale-x-100 before:transition-all before:duration-300 text-white" :class="{ 'before:!visible before:!scale-x-100': current === nav.title, '!text-[#666666] !text-[15px] !px-[20px] !py-[10px] before:!bg-white hover:!text-[#fc0324]': menuOpen && width < 1024, '!text-[#fc0324]': current === nav.title && menuOpen && width < 1024 }" @click="menuOpen = false">
+						<p v-else class="cursor-pointer flex items-center text-[14px] py-1 transition-all duration-300 relative uppercase before:content-[''] before:absolute before:w-full before:h-[2px] before:-bottom-[6px] before:left-0 before:bg-[#fc0324] before:invisible hover:before:visible before:scale-x-0 hover:before:scale-x-100 before:transition-all before:duration-300 text-white" :class="{ 'before:!visible before:!scale-x-100': current === nav.title, '!text-[#666666] !text-[15px] !px-[20px] !py-[10px] before:!bg-white hover:!text-[#fc0324]': menuOpen && width < 1024, '!text-[#fc0324]': current === nav.title && menuOpen && width < 1024 }" @click="scrollTo(nav.title)">
 							{{ nav.title }}
 						</p>
 					</li>
@@ -53,79 +97,131 @@ const current = ref('Início')
 		</div>
 	</header>
 
-	<section class="w-full h-screen bg-[url('/images/hmmbg.jpg')] bg-cover relative bg-top before:content-[''] before:absolute before:bg-[#00000099] before:bottom-0 before:top-0 before:left-0 before:right-0">
+	<section ref="inicio" class="w-full h-screen bg-[url('/images/hmmbg.jpg')] bg-cover relative bg-top before:content-[''] before:absolute before:bg-[#00000099] before:bottom-0 before:top-0 before:left-0 before:right-0">
 		<div class="absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center text-center">
 			<h1 class="mt-[30px] mb-[10px] text-[28px] md:text-[48px] font-bold leading-[36px] md:leading-[56px] uppercase text-white">
-				Hamilton Marcenario Moderna
+				Hamilton Marcenaria Moderna
 			</h1>
 			<h2 class="text-[#eee] mb-[50px] md:text-[24px] text-[18px]">
 				A melhor marcenaria do Rio de Janeiro
 			</h2>
-			<p class="uppercase text-[16px] inline-block py-[8px] px-[28px] rounded-[50px] transition-all duration-500 m-[10px] font-medium border-2 border-white text-white hover:bg-[#fc0324] hover:border-[#fc0324] cursor-pointer">
+			<p class="uppercase text-[16px] inline-block py-[8px] px-[28px] rounded-[50px] transition-all duration-500 m-[10px] font-medium border-2 border-white text-white hover:bg-[#fc0324] hover:border-[#fc0324] cursor-pointer" @click="scrollTo('Sobre')">
 				Conhecer
 			</p>
 		</div>
 	</section>
 
-	<!-- <main>
-		<section class="bg-white py-[80px]">
+	<main>
+		<section ref="sobre" class="bg-white py-1 lg:py-[80px]">
 			<div class="container mx-auto">
-				<div class="flex flex-wrap">
-					<div class="order-2 lg:order-1 lg:flex lg:w-1/2">
-						<h2 class="title">
+				<div class="flex flex-wrap items-center">
+					<div class="order-2 lg:order-1 lg:w-1/2 px-4">
+						<h2 class="text-[#333] font-bold text-[32px] pt-[15px] lg:pt-0 uppercase">
 							Sobre
 						</h2>
-						<p>
-							Aqui na Hamilton Marcenaria Moderna, nos fazemos isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo .
+						<p class="mb-[30px]">
+							Hamilton Marcenaria Moderna é uma empresa de móveis planejados em MDF, modulados, projetos sob medida e reformas em geral localizada no Rio de Janeiro.
 						</p>
-
-						<div class="icon-box">
-							<div class="icon">
-								<i class="bi bi-briefcase" />
+						<div class="bg-white bg-cover pb-[30px]">
+							<div class="flex flex-col justify-center items-center float-left bg-white w-[64px] h-[64px] text-center rounded-[50%] border-2 border-[#fc0324]">
+								<Icon name="map:furniture-store" color="#fc0324" size="30px" />
 							</div>
-							<h4 class="title">
-								<a href="">Sobre 1</a>
+							<h4 class="ms-[80px] mb-[5px] text-[18px] uppercase font-medium text-black">
+								Móveis Planejados em MDF
 							</h4>
-							<p class="description">
-								isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo
+							<p class="ms-[80px] text-[14px]">
+								Na Hamilton Marcenaria Moderna, criamos móveis planejados em MDF de alta qualidade, combinando elegância e funcionalidade para transformar visões em realidade.
 							</p>
 						</div>
-
-						<div class="icon-box">
-							<div class="icon">
-								<i class="bi bi-card-checklist" />
+						<div class="bg-white bg-cover pb-[30px]">
+							<div class="flex flex-col justify-center items-center float-left bg-white w-[64px] h-[64px] text-center rounded-[50%] border-2 border-[#fc0324]">
+								<Icon name="solar:closet-bold" color="#fc0324" size="30px" />
 							</div>
-							<h4 class="title">
-								<a href="">Sobre 2</a>
+							<h4 class="ms-[80px] mb-[5px] text-[18px] uppercase font-medium text-black">
+								Modulados Inovadores
 							</h4>
-							<p class="description">
-								isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo
+							<p class="ms-[80px] text-[14px]">
+								Descubra soluções versáteis e contemporâneas com nossos móveis modulados, projetados para otimizar espaços e adicionar um toque moderno à sua decoração.
 							</p>
 						</div>
-
-						<div class="icon-box">
-							<div class="icon">
-								<i class="bi bi-binoculars" />
+						<div class="bg-white bg-cover pb-[30px]">
+							<div class="flex flex-col justify-center items-center float-left bg-white w-[64px] h-[64px] text-center rounded-[50%] border-2 border-[#fc0324]">
+								<Icon name="entypo:tools" color="#fc0324" size="30px" />
 							</div>
-							<h4 class="title">
-								<a href="">Sobre 3</a>
+							<h4 class="ms-[80px] mb-[5px] text-[18px] uppercase font-medium text-black">
+								Reformas em Geral
 							</h4>
-							<p class="description">
-								isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo isso aquilo
+							<p class="ms-[80px] text-[14px]">
+								De renovações de móveis a remodelações completas, nossa equipe especializada em reformas transforma espaços, garantindo funcionalidade e estética excepcionais.
 							</p>
 						</div>
 					</div>
-					<NuxtImg src="/images/sobre.png" class="order-1 lg:order-2 lg:flex lg:w-1/2  min-h-[300px] mb-[10px]" />
+					<NuxtImg src="/images/sobre.png" class="order-1 lg:order-2 lg:flex w-full h-96 object-cover lg:w-1/2 lg:h-auto mb-[10px] mx-auto p-4 rounded-3xl max-h-[500px]" />
 				</div>
 			</div>
 		</section>
-	</main> -->
+
+		<section ref="orcamento" class="bg-fixed bg-center bg-cover relative py-[80px]" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/images/wppbg.jpg');">
+			<div class="container mx-auto">
+				<div class="block text-center lg:text-start lg:flex justify-between items-center">
+					<div class="text-white">
+						<h3 class="text-[28px] font-bold">
+							Faça um orçamento
+						</h3>
+						<p class="cta-text">
+							Clique no botão a direita e entre em contato pelo whatsapp
+						</p>
+					</div>
+					<NuxtLink to="https://api.whatsapp.com/send?phone=5521983833874&text=Ol%C3%A1%20Hamilton,%20gostaria%20de%20contratar%20um%20servi%C3%A7o!" external target="_blank" class="uppercase text-[16px] inline-block py-[8px] px-[28px] rounded-[50px] transition-all duration-500 m-[10px] font-medium border-2 border-white text-white hover:bg-[#fc0324] hover:border-[#fc0324] cursor-pointer mt-10 lg:mt-0">
+						Whatsapp
+					</NuxtLink>
+				</div>
+			</div>
+		</section>
+
+		<section ref="contato" class="bg-white py-1 lg:py-[80px] text-center mb-10 sm:mb-0">
+			<h2 class="text-[#333] font-bold text-[32px] pt-[15px] lg:pt-0 uppercase">
+				Contato
+			</h2>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.353524138657!2d-43.23524962514366!3d-22.90032783763937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997ef332e8cc97%3A0x9820c8f34773c58c!2sR.%20S%C3%A3o%20Luiz%20Gonzaga%2C%201064%20-%20Benfica%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2020910-062!5e0!3m2!1sen!2sbr!4v1703626146204!5m2!1sen!2sbr" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen />
+			<div class="grid grid-cols-1 sm:grid-cols-2 place-items-center mt-10 p-5">
+				<div class="max-w-[300px]">
+					<Icon name="mingcute:location-2-line" color="#fc0324" size="24px" class="inline-block" />
+					<p class="text-black inline">
+						R. São Luiz Gonzaga 1064 - Benfica, Rio de Janeiro, RJ
+					</p>
+				</div>
+				<NuxtLink to="mailto:hamilton@rosa.dev.br" class="mt-8 sm:mt-0">
+					<Icon name="ph:envelope-bold" color="#fc0324" size="24px" class="inline-block" />
+					<p class="text-black inline">
+						hamilton@rosa.dev.br
+					</p>
+				</NuxtLink>
+				<NuxtLink to="https://api.whatsapp.com/send?phone=5521983833874&text=Ol%C3%A1%20Hamilton,%20gostaria%20de%20contratar%20um%20servi%C3%A7o!" external target="_blank" class="mt-8">
+					<Icon name="ic:twotone-whatsapp" color="#fc0324" size="24px" class="inline-block" />
+					<p class="text-black inline">
+						(21) 98383-3874
+					</p>
+				</NuxtLink>
+				<NuxtLink to="tel:22998128003" class="mt-8">
+					<Icon name="bi:phone" color="#fc0324" size="24px" class="inline-block" />
+					<p class="text-black inline">
+						(22) 99812-8003
+					</p>
+				</NuxtLink>
+			</div>
+		</section>
+	</main>
 
 	<footer class="bg-[#343b40] py-[30px] font-[14px]">
 		<div class="container mx-auto text-center">
 			<span class="text-[#ccc]">Desenvolvido por <NuxtLink to="https://rosa.dev.br" external target="_blank" class="hover:text-[#F28AA9]">Gabriel Rosa</NuxtLink></span>
 		</div>
 	</footer>
+
+	<span class="md:flex justify-center items-center fixed right-[15px] bottom-[15px] z-[100] bg-[#fc0324] w-9 h-9 rounded-lg transition-all duration-500 cursor-pointer hover:bg-[#ca283e] hidden" :class="{ 'opacity-100': yScrolled, 'invisible opacity-0': !yScrolled }" @click="scrollTo('Início')">
+		<Icon name="humbleicons:arrow-up" color="white" size="24px" class="inline-block" />
+	</span>
 </template>
 
 <style>
